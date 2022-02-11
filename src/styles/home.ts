9338449f87956c865616f220d5936460c8267d67
thayrone <thayronedepaula15@gmail.com>
2022-02-11 15:50:50 -0300

    styles: change scroll and elements position

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -3,8 +3,31 @@ import styled from "styled-components";
 export const PokemonGrid = styled.ul`
   max-width: 1000px;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(168px, 1fr));
-  gap: 32px;
+  grid-template-columns: repeat(auto-fit, minmax(188px, 1fr));
+  
+  row-gap:32px;
+
+  flex: 1;
+  overflow-y: auto;
+
+  max-height: 580px;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    border-radius: 30px;
+    background-color: #E90200;
+  }
 `;
 
+export const Main = styled.main`
+  max-width: 1204px;
+  margin: 0 auto;
+`;
 
+export const MainSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
