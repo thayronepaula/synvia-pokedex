e4432ef402f9bfd6bee52f848873ff539d878823
thayrone <thayronedepaula15@gmail.com>
2022-02-11 09:10:35 -0300

    chore: function filter

--- src/components/FilterPokemons/index.tsx
+++ src/components/FilterPokemons/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Image from "next/image";
+
 import { Container, Title, TypeList, TypePokemon } from "./styles";
+
+import { useTypePokemonData } from "../../context/filterType";
+
+import CheckIcon from "../../../public/check.svg";
 
 const types = [
   "Bug",
@@ -21,15 +27,35 @@ const types = [
   "Steel",
   "Water",
 ];
+
 const index = () => {
+  const { typesPokemons, onToggleArray } = useTypePokemonData();
   return (
     <Container>
       <Title>Filtrar por tipo</Title>
 
       <TypeList>
-        {types.map((item) => (
-          <TypePokemon key={item}>{item}</TypePokemon>
-        ))}
+        {types.map((item) => {
+          const activeFilter = typesPokemons?.includes(item);
+
+          return (
+            <TypePokemon
+              key={item}
+              active={activeFilter}
+              onClick={() => onToggleArray(item)}
+            >
+              {item}
+              {activeFilter && (
+                <Image
+                  width={14}
+                  height={14}
+                  src={CheckIcon.src}
+                  alt="icon check"
+                />
+              )}
+            </TypePokemon>
+          );
+        })}
       </TypeList>
       <Title>Filtrar por tipo</Title>
     </Container>
